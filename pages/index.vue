5e1232be867e238da9d552b439a687948409bdc5
Jonathan Muñoz Morales <jonathan_tuenty@hotmail.com>
2024-10-01 00:30:52 +0200

    feat: added final view

--- pages/index.vue
+++ pages/index.vue
@@ -4,21 +4,21 @@
 </template>
 
 <script setup lang="ts">
-import * as THREE from 'three'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { ref, onMounted } from 'vue'
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { ref, onMounted } from 'vue';
 
 const loader = new GLTFLoader();
 const scene = new THREE.Scene();
-const landingLoading = ref(null);
-scene.background = new THREE.Color(0x678380);
+const landingLoading = ref<HTMLElement | null>(null);
+scene.background = new THREE.Color(0x572470);
 
 let camera: THREE.PerspectiveCamera;
 let renderer: THREE.WebGLRenderer;
 
 onMounted(() => {
-  camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.3, 1000);
-  camera.position.set(1.7, 1.3, -0.6);
+  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.3, 1000);
+  camera.position.set(1.9, 1.0, -0.6);
 
   renderer = new THREE.WebGLRenderer();
   renderer.shadowMap.enabled = true;
@@ -29,8 +29,9 @@ onMounted(() => {
   loader.load(
     new URL('public/media/room.glb', import.meta.url).href,
     (gltf) => {
-      console.log(gltf);
       gltf.scene.children[0].intensity = 10;
+      gltf.scene.children[1].visible = false;
+      gltf.scene.children[2].visible = false;
       gltf.scene.children[112].intensity = 60;
       scene.add(gltf.scene);
       renderer.render(scene, camera);
@@ -41,7 +42,7 @@ onMounted(() => {
     }
   );
 
-  camera.lookAt(-40, -10, 1);
+  camera.lookAt(-70, -5, 1); // function to rotate
 
   const animate = () => {
     requestAnimationFrame(animate);
